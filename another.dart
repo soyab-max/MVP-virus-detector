25f778c719a9cff4f68fa7a848318e7ca5a4f6e3
soyab-max <ansarisoyab9718055@gmail.com>
2025-02-15 12:17:23 +0530

    Add files via upload

---
+++ another.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class AnotherScreen extends StatefulWidget {
+  const AnotherScreen({super.key});
+
+  @override
+  State<AnotherScreen> createState() => _AnotherScreenState();
+}
+
+class _AnotherScreenState extends State<AnotherScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
